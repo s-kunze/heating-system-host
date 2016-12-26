bc7f375b9d9407e04071da152f15bcec20f1a6ef
Stefan Kunze <unforgiven248@googlemail.com>
2016-12-26 16:04:39 +0100

    - add file

---
+++ src/main/java/de/kunze/heating/host/service/HeatingConfiguration.java
@@ -0,0 +1,15 @@
+package de.kunze.heating.host.service;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "heating")
+public class HeatingConfiguration {
+
+	private String relaisids;
+	
+}
